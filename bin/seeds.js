a194e70e211676f1707fbbf7c10327e95aa906fa
Eva Francés <evafrances@MacBook-Pro-de-Eva.local>
2019-04-22 20:14:51 +0200

    incluyo seeds

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,1 +1,28 @@
 // archivo de creacion de base de datos
+const mongoose = require('mongoose');
+const Celebrity = require('../models/celebrity.js');
+require('../config/db.config.js');
+
+
+const celebArray = [
+  {
+    name: 'Tom Cruise',
+    occupation: 'actor',
+    catchPhrase: 'Danger Zone',
+  },
+  {
+    name: 'Gillian Flynn',
+    occupation: 'author',
+    catchPhrase: 'Does anyone do anything profusely except apologize? Sweat, I guess.',
+  },
+  {
+    name: 'Frank Herbert',
+    occupation: 'author',
+    catchPhrase: 'The spice must flow',
+  },
+];
+
+Celebrity.create(celebArray)
+ .then((celebArray) => console.info(`${celebArray.length} new celebArray added to the database`))
+ .catch(error => console.error(error))
+ .then(() => mongoose.connection.close());

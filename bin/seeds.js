24db89858dd88177c7751fbe0a1abda5b219b3a8
Eva Francés <evafrances@MacBook-Pro-de-Eva.local>
2019-05-01 19:14:29 +0200

    axios

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,28 +1,44 @@
 // archivo de creacion de base de datos
+const axios = require('axios');
+const Places = require('../models/place.model');
+const Valuations = require('../models/valuations.model');
+const Booking = require('../models/booking.model');
 const mongoose = require('mongoose');
-const Celebrity = require('../models/celebrity.js');
-require('../config/db.config.js');
+require('../config/db.config');
+const what = 'Restaurant'
+const lugares = ['Restaurant', 'coffee', 'bar']
+
+// Ayudar al pobre de Josué
+// lugares.map(lugar => {
+//   getLocations(lugar);
+// })
+// Conectas axios
+// Make a request for a user with a given ID
+function getLocations(what) {
+  axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json', {
+  params: {
+    query:`${what}+in+madrid`,
+    key:'AIzaSyC6ZCMc68-WlyzGtqkjraw3nroaEYqcIww'
+  }
+})
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+
+}
+
+// tratas el response (axios)
+
+// import mongo 
 
 
-const celebArray = [
-  {
-    name: 'Tom Cruise',
-    occupation: 'actor',
-    catchPhrase: 'Danger Zone',
-  },
-  {
-    name: 'Gillian Flynn',
-    occupation: 'author',
-    catchPhrase: 'Does anyone do anything profusely except apologize? Sweat, I guess.',
-  },
-  {
-    name: 'Frank Herbert',
-    occupation: 'author',
-    catchPhrase: 'The spice must flow',
-  },
-];
-
-Celebrity.create(celebArray)
- .then((celebArray) => console.info(`${celebArray.length} new celebArray added to the database`))
- .catch(error => console.error(error))
- .then(() => mongoose.connection.close());
+// Places.create(movies)
+//   .then((movies) => console.info(`${movies.length} new movies added to the database`))
+//   .catch(error => console.error(error))
+//   .then(() => mongoose.connection.close());

a0ef57c3b2bd52d11722ade4030fe04719625a98
Fran Enriquez <fjenriglez@gmail.com>
2019-05-12 21:59:39 +0200

    seeds finish

--- bin/seeds.js
+++ bin/seeds.js
@@ -3,19 +3,15 @@
 require('dotenv').config();
 const constants = require('../constants')
 
-const location=  "40.3833013, -3.6724749"
 
 const LOREM_IPSUM = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id ligula quis ipsum bibendum laoreet id ut mauris. Duis sollicitudin ac enim eget faucibus. Vestibulum at lacus eu dolor condimentum fermentum. Curabitur suscipit turpis et odio blandit dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque rhoncus justo ac sem viverra, nec dignissim nunc lobortis. Ut imperdiet quis ligula eu iaculis. Praesent interdum leo sit amet ultricies semper.'
-const NUM_COMMENTS_MAX = 10
 const NUM_USERS_CREATE = 40
-const NUM_FAVORITES_MAX = 100
 const QUERY_GOOGLE_URI = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
 const QUERY_GOOGLE_DATA = {
-  location: location,
+  location: "40.425530, -3.703252",
   radius: "10000",
   key: process.env.GOOGLE_API_KEY
 };
-
 
 const axios = require('axios');
 const User = require('../models/user.model');
@@ -29,29 +25,43 @@ const musicsType = constants.MUSIC_TYPE
 const foodsType = constants.FOOD_TYPE
 const placesType = constants.PLACE_TYPE
 
-let users = []; 
-let places = []
+const users = createUsers(NUM_USERS_CREATE); 
+let places = [];
+
+queryGoogle()
+
+function createDB() {
+  User.create(users)
+    .then((users) => console.info(`${users.length} new users added to the database`))
+    .catch(error => console.error(error + ' error1'))
+  Places.create(places)
+    .then((places) => { console.info(`${places.length} new places added to the database`) })
+    .catch(error => console.error(error + 'error2'))
+  Comments.create(createComments())
+    .then((comments) => console.info(`${comments.length} new comments added to the database`))
+    .catch(error => console.error(error + ' error3'))
+  Favorites.create(createFavorites())
+    .then((favorites) => console.info(`${favorites.length} new favorites added to the database`))
+    .catch(error => console.error(error))
+}
 
 function createUsers() {
-  for(i=0; i<40; i++) {
+  let arrUser = []
+  for (i = 0; i < 40; i++) {
     let user = new User({
       name: `user${i}`,
       email: `user${i}@example.org`,
       password: '12345678'
     })
-    users = [...users, user]
+    arrUser = [...arrUser, user]
   }
-  return users
-} 
-
-function elementAleatory(myArray) {
-  return myArray[Math.floor(Math.random()*myArray.length)]
+  return arrUser
 }
 
-User.create(createUsers(40))
-  .then((users) => console.info(`${users.length} new users added to the database`))
-  .catch(error => console.error(error))
-  
+function elementAleatory(myArray) {
+  return myArray[Math.floor(Math.random() * myArray.length)]
+}
+
 
 function createPlaces(datas, type) {
   (datas.filter(data => data.name != undefined)).forEach((data) => {
@@ -75,19 +85,8 @@ function createPlaces(datas, type) {
       localType: type,
     })
     places = [...places, place]
+    if (places.length >= placesType.length * 60) createDB()
   })
-  console.log(places)
-  Places.create(places)
-  .then((places) => {console.info(`${places.length} new places added to the database`)
-    // Comments.create(createComments(NUM_COMMENTS_MAX))
-    //   .then((comments) => console.info(`${NUM_COMMENTS_MAX} new comments added to the database`))
-    //   .catch(error => console.error(error))
-    // Favorites.create(createFavorites(NUM_FAVORITES_MAX))
-    //   .then((favorites) => console.info(`${NUM_FAVORITES_MAX} new comments added to the database`))
-    //   .catch(error => console.error(error))
-  })
-  .catch(error => console.error(error))
-  .then(() => mongoose.connection.close());
 }
 
 function sleeper(ms) {
@@ -105,53 +104,62 @@ function nextPage(data) {
 }
 
 
-placesType.forEach((type) => {
-  let results = []
-  axios.get(QUERY_GOOGLE_URI, {params: {...QUERY_GOOGLE_DATA, type: type}})
-    .then(sleeper(4000))
-    .then((response) => {
-      nextPage(response.data)
-        .then(sleeper(4000))
-        .then((response) => {
-          nextPage(response.data)
-            .then(sleeper(4000))
-            .then((response) => {
-              results = [...results, ...response.data.results]
-              return createPlaces(results, type)
-            })
-          results = [...results, ...response.data.results]
-        })
-      results = [results, ...response.data.results]
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-})
-
-function createComments(maxComments, places) {
-  return places.map(place =>{
-    for (i=0; i>=Math.floor(Math.random()*maxComments) ; i++) {
-      return {
-        title: 'title',
-        user:  elementAleatory(users.map(user=> user._id)),
-        place: elementAleatory(place[i]._id),
-        comment: LOREM_IPSUM,
-      }
-    }
+function queryGoogle() {
+  placesType.forEach((type) => {
+    let results = []
+    axios.get(QUERY_GOOGLE_URI, { params: { ...QUERY_GOOGLE_DATA, type: type } })
+      .then(sleeper(2500))
+      .then((response) => {
+        nextPage(response.data)
+          .then(sleeper(2500))
+          .then((response) => {
+            nextPage(response.data)
+              .then((response) => {
+                results = [...results, ...response.data.results]
+                return createPlaces(results, type)
+              })
+            results = [...results, ...response.data.results]
+          })
+        results = [results, ...response.data.results]
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
   })
-} 
+}
 
-
-
-function createFavorites(maxFavorites, places) {
-  return places.map(place =>{
-    for (i=0; i>=Math.floor(Math.random()*maxFavorites) ; i++) {
-      return {
-        user:  elementAleatory(users.map(user=> user._id)),
-        place: elementAleatory(place[i]._id),
+function createComments() {
+  let newComments = []
+  places.map(place => {
+    users.forEach(user => {
+      if (Math.floor(Math.random() * 2) % 2 == 0) {
+        newComments = [...newComments, {
+          title: 'Title comment of user: ' + user.name,
+          user: user._id,
+          place: place._id,
+          comment: LOREM_IPSUM,
+        }]
       }
-    }
+    })
   })
+  return newComments
+}
+
+
+function createFavorites() {
+  let newFavorites = []
+  places.forEach(place => {
+    users.forEach(user => {
+      if (Math.floor(Math.random() * 5) % 5 == 0) {
+        newFavorites = [...newFavorites, {
+          user: user._id,
+          place: place._id,
+          comment: LOREM_IPSUM,
+        }]
+      }
+    })
+  })
+  return newFavorites
 }
 
 

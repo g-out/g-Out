c00e455892dbafb0d5127047f1d43184690fbd36
Fran Enriquez <fjenriglez@gmail.com>
2019-05-12 14:43:58 +0200

    options maps

--- public/js/map.js
+++ public/js/map.js
@@ -1,145 +1,136 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZXZhZnJhbmNlcyIsImEiOiJjanY5Z3Fmc3gwb3NmM3ltanphbmhlbjJ2In0.RurxxVNW1-cb21xzVvvdeg';
-// This adds the map to your page
+
 var map = new mapboxgl.Map({
-  // container id specified in the HTML
   container: 'map',
-  // style URL
-  style: 'mapbox://styles/mapbox/light-v10',
-  // initial position in [lon, lat] format
-  center: [-3.703252, 40.425530],
-  // initial zoom
-  zoom: 12
+  style: 'mapbox://styles/mapbox/outdoors-v11',
+  zoom: 16,
+  pitch: 45,
+  bearing: -17.6,
 });
 
+navigator.geolocation.getCurrentPosition(pos=>map.setCenter([pos.coords.longitude, pos.coords.latitude]));
 
-map.on('load', function (e) {
-  // Add the data to your map as a layer
+map.on('load', mapOptions);
+
+function mapOptions() {
   map.addSource('places', {
     type: 'geojson',
     data: places,
-    cluster: true,
-    clusterMaxZoom: 14, // Max zoom to cluster points on
-    clusterRadius: 50
   });
-  // map.addLayer({
-  //   id: "clusters",
-  //   type: "circle",
-  //   source: "places",
-  //   filter: ["has", "point_count"],
-  //   paint: {
-  //     // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
-  //     // with three steps to implement three types of circles:
-  //     //   * Blue, 20px circles when point count is less than 100
-  //     //   * Yellow, 30px circles when point count is between 100 and 750
-  //     //   * Pink, 40px circles when point count is greater than or equal to 750
-  //     "circle-color": [
-  //       "step",
-  //       ["get", "point_count"],
-  //       "#51bbd6",
-  //       100,
-  //       "#f1f075",
-  //       750,
-  //       "#f28cb1"
-  //     ],
-  //     "circle-radius": [
-  //       "step",
-  //       ["get", "point_count"],
-  //       20,
-  //       100,
-  //       30,
-  //       750,
-  //       40
-  //     ]
-  //   }
-  // });
-  // map.addLayer({
-  //   id: "cluster-count",
-  //   type: "symbol",
-  //   source: "earthquakes",
-  //   filter: ["has", "point_count"],
-  //   layout: {
-  //     "text-field": "{point_count_abbreviated}",
-  //     "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
-  //     "text-size": 12
-  //   }
-  // });
+  map.addControl(new mapboxgl.NavigationControl());
+  map.setLayoutProperty('country-label', 'text-field', ['get', 'name_es'])
+
+  map.addControl(new mapboxgl.GeolocateControl({
+    positionOptions: { enableHighAccuracy: true },
+    trackUserLocation: true,
+  }));
+
+  var marker = new mapboxgl.Marker({
+    draggable: true
+  })
+    .setLngLat([-3.703252, 40.425530])
+    .addTo(map);
+
+  function onDragEnd() {
+    let lngLat = marker.getLngLat();
+    //aqui tengo las coordenadas para mandarlo
+
+  }
+
+  marker.on('dragend', onDragEnd);
+  let layers = map.getStyle().layers;
+
+  let labelLayerId;
+  for (let i = 0; i < layers.length; i++) {
+    if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+      labelLayerId = layers[i].id;
+      break;
+    }
+  }
+
+  map.addLayer({
+    'id': '3d-buildings',
+    'source': 'composite',
+    'source-layer': 'building',
+    'filter': ['==', 'extrude', 'true'],
+    'type': 'fill-extrusion',
+    'minzoom': 15,
+    'paint': {
+      'fill-extrusion-color': '#aaa',
+
+      // use an 'interpolate' expression to add a smooth transition effect to the
+      // buildings as the user zooms in
+      'fill-extrusion-height': [
+        "interpolate", ["linear"], ["zoom"],
+        15, 0,
+        15.05, ["get", "height"]
+      ],
+      'fill-extrusion-base': [
+        "interpolate", ["linear"], ["zoom"],
+        15, 0,
+        15.05, ["get", "min_height"]
+      ],
+      'fill-extrusion-opacity': .6
+    }
+  }, labelLayerId);
   buildLocationList(places);
-});
+}
 
 function buildLocationList(data) {
 
   for (i = 0; i < data.features.length; i++) {
     var currentFeature = data.features[i];
-    // Shorten data.feature.properties to `prop` so we're not
-    // writing this long form over and over again.
     var prop = currentFeature.properties;
-    // Select the listing container in the HTML and append a div
-    // with the class 'item' for each store
     var listings = document.getElementById('listings');
     var listing = listings.appendChild(document.createElement('div'));
+
     listing.className = 'item';
     listing.id = 'listing-' + i;
 
-    // Create a new link with the class 'title' for each store
-    // and fill it with the store address
     var link = listing.appendChild(document.createElement('a'));
     link.href = '#';
     link.className = 'title';
     link.dataPosition = i;
     link.innerHTML = prop.name;
 
-    // Create a new div with the class 'details' for each store
-    // and fill it with the city and phone number
     var details = listing.appendChild(document.createElement('div'));
     details.innerHTML = '<b>Address: </b>' + prop.address;
-    if (prop.phone) {
-      details.innerHTML += ' · <b>Phone: </b>' + prop.phone;
-    }
-    if (prop.description) {
-      details.innerHTML += '<p>' + prop.description + '</p>';
-    }
+    if (prop.phone) { details.innerHTML += ' · <b>Phone: </b>' + prop.phone }
+    if (prop.description) { details.innerHTML += '<p>' + prop.description + '</p>' }
 
     const userLike = prop.favorites.map(favorite => favorite.user)
     let classlike = ''
+    if (userLike.includes(prop.userLoginID)) { classlike = 'red' } else { classlike = 'black' }
 
-    if(userLike.includes(prop.userLoginID)){ classlike = 'red'} else { classlike= 'black' }
+    details.innerHTML += '<div><i class="fas fa-heart heart ' + classlike + '" data-local-id="' + prop.placeID + '" onclick="onClickLikeLocal(event)" ></i><p class="likes">' + prop.favorites.length + ' likes</p></div>'
 
-    details.innerHTML += '<div><i class="fas fa-heart heart ' + classlike + '" data-local-id="' + prop.placeID + '" onclick="onClickLikeLocal(event)" ></i><p class="likes">'+ prop.favorites.length +' likes</p></div>'
-
-    // Add an event listener for the links in the sidebar listing
     link.addEventListener('click', function (e) {
-      // Update the currentFeature to the store associated with the clicked link
       var clickedListing = data.features[this.dataPosition];
-      // 1. Fly to the point associated with the clicked link
       flyToStore(clickedListing);
-      // 2. Close all other popups and display popup for clicked store
       createPopUp(clickedListing);
-      // 3. Highlight listing in sidebar (and remove highlight for all other listings)
+
       var activeItem = document.getElementsByClassName('active');
-      if (activeItem[0]) {
-        activeItem[0].classList.remove('active');
-      }
+      if (activeItem[0]) { activeItem[0].classList.remove('active'); }
+
       this.parentNode.classList.add('active');
     });
-
   }
 }
 
 function flyToStore(currentFeature) {
   map.flyTo({
     center: currentFeature.geometry.coordinates,
-    zoom: 15
+    zoom: 17
   });
 }
 
 function createPopUp(currentFeature) {
   var popUps = document.getElementsByClassName('mapboxgl-popup');
-  // Check if there is already a popup on the map and if so, remove it
   if (popUps[0]) popUps[0].remove();
 
   var popup = new mapboxgl.Popup({
-      closeOnClick: false
-    })
+    closeOnClick: false
+  })
     .setLngLat(currentFeature.geometry.coordinates)
     .setHTML('<img src="' + currentFeature.properties.image + '"><h3 style="font-size: 22px"><a href="/local/' + currentFeature.properties.placeID + '">' + currentFeature.properties.name + '</a></h3>' +
       '<span style="color: black">' + currentFeature.properties.address + '</span>')
@@ -148,40 +139,33 @@ function createPopUp(currentFeature) {
 
 
 places.features.forEach(function (marker) {
-  // Create a div element for the marker
   var el = document.createElement('div');
-  // Add a class called 'marker' to each div
   switch (marker.type) {
     case "bar":
       el.className = 'markerBar';
-    break;
+      break;
     case "night_club":
       el.className = 'markerNight_club';
       break;
     case "cafe":
       el.className = 'markerCoffe';
-    break;
+      break;
     case "Restaurant":
       el.className = 'markerRestaurant';
-    break;
+      break;
   }
-  // By default the image for your custom marker will be anchored
-  // by its center. Adjust the position accordingly
-  // Create the custom markers, set their position, and add to map
+
   new mapboxgl.Marker(el, {
-      offset: [0, -23]
-    })
+    offset: [0, -23]
+  })
     .setLngLat(marker.geometry.coordinates)
     .addTo(map);
 
 
   el.addEventListener('click', function (e) {
     var activeItem = document.getElementsByClassName('active');
-    // 1. Fly to the point
     flyToStore(marker);
-    // 2. Close all other popups and display popup for clicked store
     createPopUp(marker);
-    // 3. Highlight listing in sidebar (and remove highlight for all other listings)
     e.stopPropagation();
     if (activeItem[0]) {
       activeItem[0].classList.remove('active');
